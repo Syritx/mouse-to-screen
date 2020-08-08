6422db424798f5b86e9e082107160987817ac6a4
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-08-08 19:15:13 -0400

    added program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace VertexClick
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            new Game(1500, 1080);
+        }
+    }
+}
